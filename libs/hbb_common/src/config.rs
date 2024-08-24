a2d7ec8439ba774f69fab75855aee935d681f5e9
hahafefef <haha@vumstar.com>
2024-08-24 15:34:41 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["fef.ffffeeg.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.wooomooo.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "JJIiSb5HwF3kLZvQoo8gt3LfzjYW87Fgft3OKcV2fOU=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
