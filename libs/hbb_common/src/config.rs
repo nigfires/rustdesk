a8c1484028869b13e82bd7c154630fe845fdd17e
hahafefef <haha@vumstar.com>
2024-08-24 18:38:01 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -2195,7 +2195,7 @@ pub mod keys {
     pub const OPTION_ENABLE_HWCODEC: &str = "enable-hwcodec";
     pub const OPTION_APPROVE_MODE: &str = "approve-mode";
     pub const OPTION_CUSTOM_RENDEZVOUS_SERVER: &str = "custom-rendezvous-server";
-    pub const OPTION_API_SERVER: &str = "http://vt.wooomooo.com";
+    pub const OPTION_CUSTOM_API_SERVER: &str = "http://vt.wooomooo.com";
     pub const OPTION_KEY: &str = "key";
     pub const OPTION_PRESET_ADDRESS_BOOK_NAME: &str = "preset-address-book-name";
     pub const OPTION_PRESET_ADDRESS_BOOK_TAG: &str = "preset-address-book-tag";
@@ -2338,7 +2338,7 @@ pub mod keys {
         OPTION_PROXY_USERNAME,
         OPTION_PROXY_PASSWORD,
         OPTION_CUSTOM_RENDEZVOUS_SERVER,
-        OPTION_CUSTOM_API_SERVER,
+        OPTION_API_SERVER,
         OPTION_KEY,
         OPTION_PRESET_ADDRESS_BOOK_NAME,
         OPTION_PRESET_ADDRESS_BOOK_TAG,
