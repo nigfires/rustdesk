7f8882633eb044befab9e43d186dbb0aed6538ee
hahafefef <haha@vumstar.com>
2024-09-14 08:17:11 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -900,7 +900,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://vt.wooomooo.com".to_owned()
+    "http://d.wooomooo.com:7000".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
